59a64f92b34b4d5470269eb816d069c505e22125
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-04-18 20:53:35 +0000

    edited

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error('Cannot proccess');
+    throw new Error('Cannot process');
   }
   map.forEach((value, key) => {
     if (value === 1) {
